ef7c087f4538954d999c41e92bc5d57207b8932d
肖余 <iseeanox@126.com>
2026-01-30 17:16:58 +0800

    登录表单输入值去空格

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,9 +2,9 @@
   <div class="loginWrapper">
     <van-form @submit="onSubmit">
       <van-cell-group inset>
-        <van-field v-model="username" name="username" label="用户名" placeholder="请输入用户名"
+        <van-field v-model.trim="username" name="username" label="用户名" placeholder="请输入用户名"
           :rules="[{ required: true, message: '请输入用户名' }]" />
-        <van-field v-model="password" type="password" name="password" label="密码" placeholder="请输入密码"
+        <van-field v-model.trim="password" type="password" name="password" label="密码" placeholder="请输入密码"
           :rules="[{ required: true, message: '请输入密码' }]" />
       </van-cell-group>
       <div class="btnWrapper">
